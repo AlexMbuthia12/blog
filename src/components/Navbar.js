c8dbab2a875f5642f59cea3543d6d41b99d570f0
Alex <alabamagreenlee@gmail.com>
2024-05-09 12:58:25 +0300

    added navbar and Navbar function

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+const Navbar = () => {
+  
+  return (
+    <div class='navbar'>
+       <nav>
+         <ul>
+            <li>Home</li>
+            <li>Create</li>
+         </ul>
+        </nav> 
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/test.js
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+
+// const Navbar = () => {
+  
+//   return (
+//     <div class='navbar'>
+//         <h1>Home</h1>
+//         <h2>Create</h2>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
